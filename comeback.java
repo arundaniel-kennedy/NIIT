8b33291b82421ff5bc29918196fbb383b446005c
arundaniel-kennedy <arundanielkennedy@gmail.com>
2020-09-16 12:25:05 +0530

    hkj

---
+++ comeback.java
@@ -0,0 +1,11 @@
+import java.io.*;
+import java.util.*;
+
+public class comeback{
+  public static void main(String[] args){
+    Scanner s = new Scanner(System.in);
+    int a = s.nextInt();
+    System.out.println(a);
+    s.close();
+  }
+}
